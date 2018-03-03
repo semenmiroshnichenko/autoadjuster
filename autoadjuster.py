d8b5d9539b2112b915ee90aabcb84e5521ee6333
Semen <semenmiroshnichenko@gmail.com>
2018-03-03 23:17:03 +0100

    Use matchTemplate() method to determine picture shift. Swith to new reference image when pixel shift is too big. Add visualisation.

--- autoadjuster.py
+++ autoadjuster.py
@@ -20,36 +20,69 @@ def getImageDate(filename):
     return datetime.datetime.strptime(exifs['DateTimeOriginal'], "%Y:%m:%d %H:%M:%S")
 
 # set this paratemer according to your objective lens and sensor you're using
-pixelScaleInArcsecPerPixel = 7.01
+pixelScaleInArcsecPerPixel = 7.29
+# pixelScaleInArcsecPerPixel = 23.4
 
 if len(sys.argv) < 2:
     sys.exit('Usage: %s path-to-your-jpeg-files' % sys.argv[0])
 
 directory = sys.argv[1]
-
 files = sorted(os.listdir(directory))
-firstFileDateTime = getImageDate(os.path.join(directory, files[0]))
+previousPixelShift = 0
+
 with open(os.path.join(directory,'results.csv'), 'wb') as csvfile:
     resultWriter = csv.writer(csvfile, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    firstFileDateTime = getImageDate(os.path.join(directory, files[0]))
+    referenceImg = cv2.imread(os.path.join(directory, files[0]), cv2.IMREAD_GRAYSCALE | cv2.IMREAD_IGNORE_ORIENTATION)
     for i in range(1, len(files)):
         if not files[i].lower().endswith(".jpg"): 
             continue
 
         fullpath = os.path.join(directory, files[i])
         fileDate = getImageDate(fullpath)
-        src1 = cv2.imread(os.path.join(directory, files[0]),0)
-        src2 = cv2.imread(os.path.join(directory, files[i]),0) 
-        src1np = np.float32(src1)  
-        src2np = np.float32(src2) 
-        ret, response = cv2.phaseCorrelate(src1np,src2np)
-        
-        pixelShift = math.sqrt(math.pow(ret[0], 2) + math.pow(ret[1], 2))
-        angleShiftInDegrees = math.degrees(math.atan(ret[1] / ret[0]))
+        actualImg = cv2.imread(os.path.join(directory, files[i]), cv2.IMREAD_GRAYSCALE | cv2.IMREAD_IGNORE_ORIENTATION) 
+        # src1np = np.float32(src1)  
+        # src2np = np.float32(src2) 
+        # ret, response = cv2.phaseCorrelate(src1np,src2np)
+        # print "phaseCorrelate result {0} {1}".format(ret[0], ret[1])
+        # pixelShift = math.sqrt(math.pow(ret[0], 2) + math.pow(ret[1], 2))
+        # angleShiftInDegrees = math.degrees(math.atan(ret[1] / ret[0]))
 
-        angleDiff = pixelShift * pixelScaleInArcsecPerPixel / 3600
-        #print angleDiff
+        # angleDiff = pixelShift * pixelScaleInArcsecPerPixel / 3600
+        # #print angleDiff
+        # timediff = fileDate - firstFileDateTime
+        # errorInDegrees = angleDiff - timediff.seconds * anglePerSecInArcDegrees
+        # print "phaseCorrelate at {0} tracking error {1} arcsec, shift {2} deg".format(timediff, errorInDegrees * 3600, angleShiftInDegrees)
+        # resultWriter.writerow([timediff, errorInDegrees * 3600, angleShiftInDegrees])
+
+        # matchTemplate
+        templateSize = 2000
+        shape = np.shape(referenceImg)
+        y = int(shape[0] / 2) - int(templateSize / 2)
+        x = int(shape[1] / 2) - int(templateSize / 2)
+        template = referenceImg[y:y + templateSize, x: x + templateSize]
+        cv2.imshow("template", cv2.resize(template, (0,0), fx=0.2, fy=0.2))
+        matchTemplateResult = cv2.matchTemplate(actualImg, template, cv2.TM_CCOEFF_NORMED)
+        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(matchTemplateResult)
+        
+        diff = (maxLoc[0] - x, maxLoc[1] - y)
+        print "matchTemplate pixel shift x:{0} y:{1}".format(diff[0], diff[1])
+        src2copy = actualImg.copy()
+        cv2.rectangle(src2copy, maxLoc, (maxLoc[0] + template.shape[0], maxLoc[1] + template.shape[1]) ,(255,255,255), 10)
+        cv2.imshow("result", cv2.resize(src2copy, (0,0), fx=0.1, fy=0.1))
+        cv2.waitKey(20)
+        pixelShift = math.sqrt(math.pow(diff[0], 2) + math.pow(diff[1], 2))
+        angleShiftInDegrees = math.degrees(math.atan(diff[1] / diff[0]))
+
+        angleDiff = (previousPixelShift + pixelShift) * pixelScaleInArcsecPerPixel / 3600
         timediff = fileDate - firstFileDateTime
         errorInDegrees = angleDiff - timediff.seconds * anglePerSecInArcDegrees
-        print "at {0} tracking error {1} arcsec, shift {2} deg".format(timediff, errorInDegrees * 3600, angleShiftInDegrees)
+        print "matchTemplate at {0} tracking error {1} arcsec, shift {2} deg".format(timediff, errorInDegrees * 3600, angleShiftInDegrees)
         resultWriter.writerow([timediff, errorInDegrees * 3600, angleShiftInDegrees])
+
+
+        if pixelShift > templateSize / 2:
+            referenceImg = cv2.imread(os.path.join(directory, files[i]), cv2.IMREAD_GRAYSCALE | cv2.IMREAD_IGNORE_ORIENTATION)
+            previousPixelShift = pixelShift
+
     print 'Done!'
