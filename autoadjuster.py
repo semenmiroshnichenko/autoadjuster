f509da21492b21a4e76d79bb48410a5456de766f
Semen <semenmiroshnichenko@gmail.com>
2018-08-02 21:56:35 +0200

    Return back to phaseCorrelate()

--- autoadjuster.py
+++ autoadjuster.py
@@ -38,8 +38,9 @@ with open(os.path.join(directory,'results.csv'), 'wb') as csvfile:
     referenceImg = cv2.imread(os.path.join(directory, files[0]), cv2.IMREAD_GRAYSCALE | cv2.IMREAD_IGNORE_ORIENTATION)
     
     shape = np.shape(referenceImg)
-    templateSize = min(shape[0], shape[1]) / 4
-    print "Template size {0} px".format(templateSize)
+    imageSize = min(shape[0], shape[1])
+    #templateSize = min(shape[0], shape[1]) / 4
+    #print "Template size {0} px".format(templateSize)
 
 
     
@@ -51,19 +52,25 @@ with open(os.path.join(directory,'results.csv'), 'wb') as csvfile:
         fileDate = getImageDate(fullpath)
         actualImg = cv2.imread(os.path.join(directory, files[i]), cv2.IMREAD_GRAYSCALE | cv2.IMREAD_IGNORE_ORIENTATION) 
 
-        y = int(shape[0] / 2) - int(templateSize / 2)
-        x = int(shape[1] / 2) - int(templateSize / 2)
-        template = referenceImg[y:y + templateSize, x: x + templateSize]
-        cv2.imshow("template", cv2.resize(template, (0,0), fx=0.2, fy=0.2))
-        matchTemplateResult = cv2.matchTemplate(actualImg, template, cv2.TM_CCOEFF_NORMED)
-        minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(matchTemplateResult)
+        #y = int(shape[0] / 2) - int(templateSize / 2)
+        #x = int(shape[1] / 2) - int(templateSize / 2)
+        #template = referenceImg[y:y + templateSize, x: x + templateSize]
+        #cv2.imshow("template", cv2.resize(template, (0,0), fx=0.2, fy=0.2))
+        #matchTemplateResult = cv2.matchTemplate(actualImg, template, cv2.TM_CCOEFF_NORMED)
+        #minVal, maxVal, minLoc, maxLoc = cv2.minMaxLoc(matchTemplateResult)
+        src1np = np.float32(referenceImg)  
+        src2np = np.float32(actualImg) 
+        ret, response = cv2.phaseCorrelate(src1np,src2np)
+        pixelShift = math.sqrt(math.pow(ret[0], 2) + math.pow(ret[1], 2))
+
         
-        diff = (maxLoc[0] - x, maxLoc[1] - y)
+        #diff = (maxLoc[0] - x, maxLoc[1] - y)
+        diff = (ret[0], ret[1])
         print "matchTemplate pixel shift x:{0} y:{1}".format(diff[0], diff[1])
-        src2copy = actualImg.copy()
-        cv2.rectangle(src2copy, maxLoc, (maxLoc[0] + template.shape[0], maxLoc[1] + template.shape[1]) ,(255,255,255), 10)
-        cv2.imshow("result", cv2.resize(src2copy, (0,0), fx=0.1, fy=0.1))
-        cv2.waitKey(20)
+        #src2copy = actualImg.copy()
+        #cv2.rectangle(src2copy, maxLoc, (maxLoc[0] + template.shape[0], maxLoc[1] + template.shape[1]) ,(255,255,255), 10)
+        #cv2.imshow("result", cv2.resize(src2copy, (0,0), fx=0.1, fy=0.1))
+        #cv2.waitKey(20)
         pixelShift = math.sqrt(math.pow(diff[0], 2) + math.pow(diff[1], 2))
         yAxisPixelShift = previousYPixelShift + diff[1]
         angleShiftInDegrees = math.degrees(math.atan(float(diff[1]) / float(diff[0])))
@@ -75,7 +82,8 @@ with open(os.path.join(directory,'results.csv'), 'wb') as csvfile:
         resultWriter.writerow([timediff, errorInDegrees * 3600, yAxisPixelShift])
 
 
-        if pixelShift > templateSize / 2:
+        #if pixelShift > templateSize / 2:
+        if pixelShift > imageSize / 4:
             referenceImg = cv2.imread(os.path.join(directory, files[i]), cv2.IMREAD_GRAYSCALE | cv2.IMREAD_IGNORE_ORIENTATION)
             previousPixelShift = previousPixelShift + pixelShift
             previousYPixelShift = yAxisPixelShift
